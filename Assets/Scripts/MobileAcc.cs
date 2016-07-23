06fe3c9f64c4a905c8eeda61a67b707838fe3097
mingchaoyan <mingchaoyan@gmail.com>
2016-07-23 15:43:28 +0800

    重力感应

---
+++ Assets/Scripts/MobileAcc.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MobileAcc : MonoBehaviour {
+	void OnGUI() {
+		GUI.Box(new Rect(5, 5, 100, 50), string.Format("{0:0.00}", Input.acceleration.x));
+		GUI.Box(new Rect(5, 60, 100, 50), string.Format("{0:0.00}", Input.acceleration.y));
+		GUI.Box(new Rect(5, 115, 100, 50), string.Format("{0:0.00}", Input.acceleration.z));
+	}
+
+}
