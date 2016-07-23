f3abb8f66e4238632b536ec0c839b24b713baea5
mingchaoyan <mingchaoyan@gmail.com>
2016-07-24 00:22:20 +0800

    使用触摸屏控制射击

---
+++ Assets/Scripts/MissileAction.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissileAction : MonoBehaviour {
+	public GameObject explosion;
+	void FixedUpdate() {
+		GetComponent<Rigidbody>().AddForce(transform.TransformDirection(Vector3.forward)* 200.0f);
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		ContactPoint contact = collision.contacts[0];
+		GameObject exp = Instantiate(explosion, contact.point + (contact.normal*5.0f),
+			Quaternion.identity) as GameObject;
+		if(collision.gameObject.tag == "enemy") {
+			Destroy(collision.gameObject);
+		}
+		Destroy(exp, 2.0f);
+		Destroy(gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
---
+++ Assets/Scripts/MissileStarter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissileStarter : MonoBehaviour {
+	public GameObject missile;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+			Vector3 position = new Vector3(0.0f, -0.2f, 1.0f) * 10.0f;
+			position = transform.TransformPoint(position);
+			GameObject thisMissile = Instantiate(missile, position, transform.rotation) as GameObject;
+			Physics.IgnoreCollision(thisMissile.GetComponent<Collider>(), GetComponent<Collider>());
+		}
+	
+	}
+}
