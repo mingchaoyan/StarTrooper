ef7adf1e3e18dc665fffe6c4f15f843cbbe1e214
mingchaoyan <mingchaoyan@gmail.com>
2016-07-23 15:06:51 +0800

    添加SunRing对象旋转

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+	public float rotateSpeed = 10.0f;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.forward, rotateSpeed* Time.deltaTime);	
+	}
+}
